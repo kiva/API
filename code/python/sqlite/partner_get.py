bab38db3f3acc9e00aa3ba68ae96179dff346ec3
coderintherye <kobrien@kiva.org>
2013-09-02 05:09:53 -0700

    slighty better messaging

--- code/python/sqlite/partner_get.py
+++ code/python/sqlite/partner_get.py
@@ -9,6 +9,7 @@ soup = BeautifulSoup(response)
 # if you have lxml installed then uncomment and use the next line
 #soup = BeautifulSoup(response, "lxml")
 lines=[]
+# establish the connection to the sqlite db
 conn = sqlite3.connect('kiva')
 c = conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS partners (id tinyint primary key, name varchar(255), status varchar(10), rating varchar(20), due_dilegence_type varchar(12), start_date datetime, delinquency_rate float, default_rate float, total_amount_raised unsigned bigint, loans_posted smallint, portfolio_yield real)');
@@ -16,6 +17,7 @@ count = 0
 # The [1:] syntax skips the first header row when we loop through all tr rows
 for partner in soup.find_all('tr')[1:]:
 	for td in partner:
+		# i dont want all the data so purposefully limit the rows
 		if(count < 11):
 			text=td.renderContents().decode('utf-8')
 			lines.append(text)
@@ -24,3 +26,4 @@ for partner in soup.find_all('tr')[1:]:
 	lines=[]
 	count = 0
 conn.commit()
+conn.close()
